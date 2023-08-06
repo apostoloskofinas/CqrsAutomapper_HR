9cec42bc34ad270a6f0b11c3b1fcbdad8a479d9e
Apostolos Kofinas <kofinas.apostolos@gmail.com>
2023-08-06 20:12:57 +0300

    Implement Repositories

--- src/Core/HR.LeaveManagement.Application/Contracts/Persistance/IGenericRepository.cs
+++ src/Core/HR.LeaveManagement.Application/Contracts/Persistance/IGenericRepository.cs
@@ -1,9 +1,9 @@
 ﻿namespace HR.LeaveManagement.Application.Contracts.Persistance;
 public interface IGenericRepository<T> where T : class
 {
-    Task<List<T>> GetAsync();
+    Task<IReadOnlyList<T>> GetAsync();
     Task<T> GetByIdAsync(int id);
-    Task<T> CreateAsync(T entity);
-    Task<T> UpdateAsync(T entity);
-    Task<T> DeleteAsync(T entity);    
+    Task CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);    
 }
