754e78091e88e8eb8411ebd30b9abd619e608e1a
akofiinas <a.kofinas@cloudf.in>
2023-09-21 16:23:06 +0300

    Add third party email service using "SendGrid"

--- HR.LeaveManagement.Infrastucture/InfrastructureServicesRegistration.cs
+++ HR.LeaveManagement.Infrastucture/InfrastructureServicesRegistration.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using HR.LeaveManagement.Infrastucture.EmailService;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HR.LeaveManagement.Infrastucture;
 public static class InfrastructureServicesRegistration
 {
-    public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection ConfigureInfrastructureServices
+            (this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailSender, EmailSender>();
         return services;
     }
 }
